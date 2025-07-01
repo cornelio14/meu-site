a6b21f7261971b27e4a3d3d2e9c33fe5ca4e32e6
cornelio14 <127529513+cornelio14@users.noreply.github.com>
2025-07-01 20:58:05 +0200

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.scss'
-import { Analytics } from '@vercel/analytics/react'
 
 // Initialize Stripe once during application startup (não precisa fazer alterações se não tiver código de inicialização global aqui)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <Analytics />
   </StrictMode>,
 )
